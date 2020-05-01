a379d697176c357265e857b2c8056764e99270c5
FlorianLaboeck <46341669+FlorianLaboeck@users.noreply.github.com>
2020-05-01 12:59:23 +0200

    created app header and todo list components

--- ravenholm/src/app/app.module.ts
+++ ravenholm/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { TodoListComponent } from './components/todo/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ ravenholm/src/app/components/todo/todo-list/todo-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.sass']
+})
+export class TodoListComponent implements OnInit {
+
+  todoItems: Array<string> = ['1', '2'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
